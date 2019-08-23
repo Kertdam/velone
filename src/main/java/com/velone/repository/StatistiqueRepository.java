1401276564dd49fe83a4ede57e8211de4d2309c1
Yekhlef <f.yekhlef@gmail.com>
2019-08-23 17:32:54 +0200

    Création repo/service Statistique.
    CRUD et getByMembreId

---
+++ src/main/java/com/velone/repository/StatistiqueRepository.java
@@ -0,0 +1,13 @@
+package com.velone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.velone.entity.Statistique;
+
+@Repository
+public interface StatistiqueRepository extends JpaRepository<Statistique, Integer> {
+
+	Statistique findByMembreId(int id_user, int id);
+
+}
