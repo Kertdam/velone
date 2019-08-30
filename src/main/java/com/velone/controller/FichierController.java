9176fe6515f6d853ed3ed4e6e2b855c6a30987b6
Yekhlef <f.yekhlef@gmail.com>
2019-08-30 12:01:30 +0200

    Fix CrossOrigin

--- src/main/java/com/velone/controller/FichierController.java
+++ src/main/java/com/velone/controller/FichierController.java
@@ -3,6 +3,7 @@ package com.velone.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.velone.entity.Fichier;
 import com.velone.service.FichierService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/fichiers")
 public class FichierController {
