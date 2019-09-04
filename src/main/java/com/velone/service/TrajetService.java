c93d94bdb727df7e511d1f0cf820c597903a7a10
Yekhlef <f.yekhlef@gmail.com>
2019-09-04 10:15:35 +0200

    Replacement du code métier du controller vers le service
    methode getTrajetsByMembreId

--- src/main/java/com/velone/service/TrajetService.java
+++ src/main/java/com/velone/service/TrajetService.java
@@ -1,18 +1,23 @@
 package com.velone.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.velone.entity.Membre;
 import com.velone.entity.Trajet;
+import com.velone.exception.ResourcesNotFoundException;
 import com.velone.repository.TrajetRepository;
 
 @Service
 public class TrajetService {
 	@Autowired
 	TrajetRepository repository;
+	@Autowired
+	MembreService serviceMembre;
 
 	public void add(Trajet trajet) {
 		repository.save(trajet);
@@ -36,6 +41,27 @@ public class TrajetService {
 
 	public Optional<Trajet> getTrajetById(Integer id) {
 		return repository.findById(id);
+	}
+
+	public List<Trajet> getTrajetsByMembreId(Integer id) {
+
+		List<Membre> listMembre = serviceMembre.getMembresByUtilisateurId(id);
+		List<Integer> listTrajetId = new ArrayList<>();
+
+		// Recup l'id des trajets dans la liste de membre
+		for (Membre membre : listMembre) {
+			Integer TrajetId = membre.getTrajet().getId();
+			listTrajetId.add(TrajetId);
+		}
+		List<Trajet> listTrajet = new ArrayList<>();
+
+		// Recup trajet grâce à la liste des TrajetId
+		for (Integer trajetId : listTrajetId) {
+			Trajet trajet = getTrajetById(trajetId)
+					.orElseThrow(() -> new ResourcesNotFoundException("trajet", "id", trajetId));
+			listTrajet.add(trajet);
+		}
+		return listTrajet;
 	}
 
 	public void updateById(Integer id, Trajet trajet) {
