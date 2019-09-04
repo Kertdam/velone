b108bb3a5fd4e4303d13d4645a8d951e5cc8ed2b
Yekhlef <f.yekhlef@gmail.com>
2019-09-04 15:26:04 +0200

    Commentaire Trajet Service

--- src/main/java/com/velone/service/TrajetService.java
+++ src/main/java/com/velone/service/TrajetService.java
@@ -19,40 +19,48 @@ public class TrajetService {
 	@Autowired
 	MembreService serviceMembre;
 
+	// Méthode qui ajoute un trajet
 	public void add(Trajet trajet) {
 		repository.save(trajet);
 	}
 
+	// Méthode qui supprime un trajet
 	public void delete(Integer id) {
 		repository.deleteById(id);
 	}
 
+	// Méthode qui récupère tous les trajets
 	public List<Trajet> getTrajets() {
 		return repository.findAll();
 	}
 
+	// Méthode qui récupère des trajets par l'utilisateur ID
 	public List<Trajet> getTrajetsByUtilisateurId(Integer id) {
 		return repository.findByUtilisateurId(id);
 	}
 
+	// Méthode qui récupère un trajet par son nom
 	public List<Trajet> getTrajetsByNom(String nom) {
 		return repository.findByNomContainsIgnoreCase(nom);
 	}
 
+	// Méthode qui récupère un trajet par son ID en base de donnée
 	public Optional<Trajet> getTrajetById(Integer id) {
 		return repository.findById(id);
 	}
 
+	// Méthode qui recupère une liste de trajet dont l'utilisateur est membre
 	public List<Trajet> getTrajetsByMembreId(Integer id) {
 
 		List<Membre> listMembre = serviceMembre.getMembresByUtilisateurId(id);
 		List<Integer> listTrajetId = new ArrayList<>();
 
-		// Recup l'id des trajets dans la liste de membre
+		// Recupération de l'id des trajets dans la liste de membre
 		for (Membre membre : listMembre) {
 			Integer TrajetId = membre.getTrajet().getId();
 			listTrajetId.add(TrajetId);
 		}
+
 		List<Trajet> listTrajet = new ArrayList<>();
 
 		// Recup trajet grâce à la liste des TrajetId
@@ -61,9 +69,11 @@ public class TrajetService {
 					.orElseThrow(() -> new ResourcesNotFoundException("trajet", "id", trajetId));
 			listTrajet.add(trajet);
 		}
+
 		return listTrajet;
 	}
 
+	// Mise à jour d'un trajet
 	public void updateById(Integer id, Trajet trajet) {
 		Trajet trajetToUpdate = repository.getOne(id);
 		trajetToUpdate.setNom(trajet.getNom());
